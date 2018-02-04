679cf169bb6352cd71f1f949f5bab876e32dd4c7
Daisuke Shimamoto <diskshima@gmail.com>
2018-02-05 00:48:38 +0900

    Clean up types in utils

--- functions/src/utils.ts
+++ functions/src/utils.ts
@@ -1,5 +1,5 @@
-export const to = (promise: Promise<any>) =>
+export const to = (promise: Promise<any>): Promise<Array<any>> =>
   promise.then(data => [null, data])
          .catch(err => [err]);
 
-export const isObject = (obj: any) => obj === Object(obj);
+export const isObject = (obj): boolean => obj === Object(obj);
