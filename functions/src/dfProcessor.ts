215a0af0b6d61ca69782c99de7d80c1294736e5c
Daisuke Shimamoto <diskshima@gmail.com>
2018-11-25 23:58:38 +0900

    Update dialogflow code for v2

--- functions/src/dfProcessor.ts
+++ functions/src/dfProcessor.ts
@@ -1,5 +1,6 @@
-import { DialogflowApp } from 'actions-on-google';
-import { getBidAsk, toFullSym } from './tickers';
+import * as  functions from 'firebase-functions';
+import { dialogflow } from 'actions-on-google';
+import { Exchange, getBidAsk, toFullSym } from './tickers';
 
 process.env.DEBUG = 'actions-on-google:*';
 
@@ -7,20 +8,15 @@ const QUOTE_ACTION = 'quote_action';
 const MARKET_ARG = 'market';
 const EXCHANGE_ARG = 'exchange';
 
-const quoteAction = async (app) => {
-  const sym = app.getArgument(MARKET_ARG).toUpperCase();
-  const exchangeName = app.getArgument(EXCHANGE_ARG).toLowerCase();
+const app = dialogflow();
+app.intent(QUOTE_ACTION, async conv => {
+  const sym = conv.parameters[MARKET_ARG].toString().toUpperCase();
+  const exchangeName = conv.parameters[EXCHANGE_ARG].toString().toLowerCase();
   const processedSym = toFullSym(sym);
 
-  const bidAsk = await getBidAsk(exchangeName, processedSym);
+  const bidAsk = await getBidAsk(exchangeName as Exchange, processedSym);
 
-  app.tell(`Ask is ${bidAsk.ask}, bid is ${bidAsk.bid} for ${sym} on ${exchangeName}`);
-};
+  conv.close(`Ask is ${bidAsk.ask}, bid is ${bidAsk.bid} for ${sym} on ${exchangeName}`);
+});
 
-const ACTION_MAP = new Map();
-ACTION_MAP.set(QUOTE_ACTION, quoteAction);
-
-export const handleDialogFlowRequest = async (request, response) => {
-  const dfApp = new DialogflowApp({ request, response });
-  dfApp.handleRequest(ACTION_MAP);
-};
+export const handleDialogFlowRequest = functions.https.onRequest(app);
